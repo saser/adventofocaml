7f8f396ef4bce5eac4ba89a9075281d506d62ee7
Christian Persson <582273+saser@users.noreply.github.com>
2025-09-20 22:15:29 +0200

    Add solution and basic benchmarking for year 2024, day 01.
    This is the first complete solution I have with OCaml, yay!

--- bin/benchmark.ml
+++ bin/benchmark.ml
@@ -4,5 +4,9 @@ open Core_bench
 let () =
   Command_unix.run
     (Bench.make_command
-       [ Bench.Test.create ~name:"sum_10k" (fun () -> ignore Hello_benchmarks.sum_10k) ])
+       [ Bench.Test.create ~name:"year2024_day01_part1" (fun () ->
+           ignore Year2024_day01.(part1 full_input))
+       ; Bench.Test.create ~name:"year2024_day01_part2" (fun () ->
+           ignore Year2024_day01.(part2 full_input))
+       ])
 ;;
