d591eeff82fb6659685b83d51cd7d4f986cee349
Christian Persson <582273+saser@users.noreply.github.com>
2025-10-01 23:33:48 +0200

    Remove "Hello, world" style code that's no longer necessary.

--- lib/hello_benchmarks.ml
+++
@@ -1,7 +0,0 @@
-let sum_10k =
-  let i = ref 0 in
-  for j = 1 to 10_000 do
-    i := !i + j
-  done;
-  !i
-;;
--- lib/hello_expect.ml
+++
@@ -1,4 +0,0 @@
-let%expect_test "greet" =
-  print_endline "Hello, expect tests!";
-  [%expect {| Hello, expect tests! |}]
-;;
