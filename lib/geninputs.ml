1993e17df57277a5faf8757b74d22e695638b05f
Christian Persson <582273+saser@users.noreply.github.com>
2025-11-12 22:37:24 +0100

    Fix build error for `Inputs` module.
    The build error was that `open Base` was unused; it shouldn't be since I should
    strip the string of the input so that solutions downstream don't have to do
    that.

--- lib/geninputs.ml
+++ lib/geninputs.ml
@@ -6,5 +6,8 @@ let () =
   Array.sort files ~compare:String.compare;
   print_endline "open Base";
   Array.iter files ~f:(fun filename ->
-    printf "let %s = {| %s |}\n\n" filename (In_channel.read_all ("inputs/" ^ filename)))
+    printf
+      "let %s = String.strip {| %s |}\n\n"
+      filename
+      (In_channel.read_all ("inputs/" ^ filename)))
 ;;
