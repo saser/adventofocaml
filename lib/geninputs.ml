2df8dbf94f53b3794766c423be16f6681f684351
Christian Persson <582273+saser@users.noreply.github.com>
2025-12-02 12:10:33 +0100

    lib/geninputs: ignore hidden files.
    This is so that the `.keep` file is ignored for code generation. Also use `open!
    Base` so that builds succeed even when there are no inputs in the directory
    (useful for building a fresh clone, for example).

--- lib/geninputs.ml
+++ lib/geninputs.ml
@@ -2,9 +2,11 @@ open Base
 open Stdio
 
 let () =
-  let files = Sys_unix.readdir "inputs" in
+  let files =
+    Sys_unix.readdir "inputs" |> Array.filter ~f:(fun file -> Char.(file.[0] <> '.'))
+  in
   Array.sort files ~compare:String.compare;
-  print_endline "open Base";
+  print_endline "open! Base";
   Array.iter files ~f:(fun filename ->
     printf
       "let %s = String.strip {| %s |}\n\n"
