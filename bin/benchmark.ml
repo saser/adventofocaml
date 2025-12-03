907aa9463f4f66243bd58455407cfc3561a30ac8
Christian Persson <582273+saser@users.noreply.github.com>
2025-12-03 20:15:38 +0100

    bin/benchmark: list all current solutions.
    I should probably find a way to generate this code or something, but for now
    updating once per problem works fine.

--- bin/benchmark.ml
+++ bin/benchmark.ml
@@ -4,7 +4,8 @@ open Core_bench
 let () =
   Command_unix.run
     (Bench.make_command
-       [ Bench.Test.create ~name:"year2024_day01_part1" (fun () ->
+       [ (* Start of 2024. *)
+         Bench.Test.create ~name:"year2024_day01_part1" (fun () ->
            ignore @@ Year2024_day01.part1 Inputs.year2024_day01)
        ; Bench.Test.create ~name:"year2024_day01_part2" (fun () ->
            ignore @@ Year2024_day01.part2 Inputs.year2024_day01)
@@ -12,9 +13,26 @@ let () =
            ignore @@ Year2024_day02.part1 Inputs.year2024_day02)
        ; Bench.Test.create ~name:"year2024_day02_part2" (fun () ->
            ignore @@ Year2024_day02.part2 Inputs.year2024_day02)
+       ; Bench.Test.create ~name:"year2024_day05_part1" (fun () ->
+           ignore @@ Year2024_day05.part1 Inputs.year2024_day05)
+       ; Bench.Test.create ~name:"year2024_day05_part2" (fun () ->
+           ignore @@ Year2024_day05.part2 Inputs.year2024_day05)
+       ; Bench.Test.create ~name:"year2024_day07_part1" (fun () ->
+           ignore @@ Year2024_day07.part1 Inputs.year2024_day07)
+       ; Bench.Test.create ~name:"year2024_day07_part2" (fun () ->
+           ignore @@ Year2024_day07.part2 Inputs.year2024_day07)
+         (* Start of 2025. *)
        ; Bench.Test.create ~name:"year2025_day01_part1" (fun () ->
            ignore @@ Year2025_day01.part1 Inputs.year2025_day01)
        ; Bench.Test.create ~name:"year2025_day01_part2" (fun () ->
            ignore @@ Year2025_day01.part2 Inputs.year2025_day01)
+       ; Bench.Test.create ~name:"year2025_day02_part1" (fun () ->
+           ignore @@ Year2025_day02.part1 Inputs.year2025_day02)
+       ; Bench.Test.create ~name:"year2025_day02_part2" (fun () ->
+           ignore @@ Year2025_day02.part2 Inputs.year2025_day02)
+       ; Bench.Test.create ~name:"year2025_day03_part1" (fun () ->
+           ignore @@ Year2025_day03.part1 Inputs.year2025_day03)
+       ; Bench.Test.create ~name:"year2025_day03_part2" (fun () ->
+           ignore @@ Year2025_day03.part2 Inputs.year2025_day03)
        ])
 ;;
