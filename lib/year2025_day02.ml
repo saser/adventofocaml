35c85e724d8baf237536315e2dc6625f24471b66
Christian Persson <582273+saser@users.noreply.github.com>
2025-12-07 22:15:22 +0100

    Year2025_day02: refactor to simpler and faster solution.
    The old one ran in ~186ms for part 1 and ~771ms for part 2; the new one runs in
    ~87ms and ~427ms respectively. It's still brute force, but this is much neater.

--- lib/year2025_day02.ml
+++ lib/year2025_day02.ml
@@ -1,26 +1,6 @@
 open Base
 open Stdio
 
-(** [count_digits x] returns the number of digits of [x] in base 10. *)
-let count_digits x =
-  Float.of_int x |> Float.log10 |> Float.round_down |> Int.of_float |> ( + ) 1
-;;
-
-(** [take_digits x ~n] returns an integer consisting of the first [n] digits of
-[x]. Raises if [n > count_digits x]. *)
-let take_digits x ~n = x / (10 ** (count_digits x - n))
-
-(** [repeat_digits x ~n] returns the integer formed by repeating the digits of
-[x] [n] times. *)
-let repeat_digits x ~n =
-  let d = 10 ** count_digits x in
-  let acc = ref 0 in
-  for _ = 0 to n - 1 do
-    acc := (!acc * d) + x
-  done;
-  !acc
-;;
-
 (** [parse_range s] parses [s] as a string of the form ["x-y"] and returns [x,
 y]. *)
 let parse_range s =
@@ -29,168 +9,75 @@ let parse_range s =
   | _ -> Printf.failwithf "invalid line: %S" s ()
 ;;
 
-(** [is_invalid x ~n] determines whether [x] consists of [n] repeated groups of digits. *)
-let is_invalid x ~n =
-  let dx = count_digits x in
-  if dx % n <> 0 then false else repeat_digits (take_digits x ~n:(dx / n)) ~n = x
+let is_repeated_exactly_twice n =
+  let s = Int.to_string n in
+  let mid = String.length s / 2 in
+  String.(subo s ~len:mid = subo s ~pos:mid)
 ;;
 
-(** [invalid_ids ~min ~max ~nmin ~nmax] returns the set of invalid IDs between
-[min] and [max] (inclusive) formed by dividing into at least [nmin] and at most
-[nmax] groups. The returned list may contain duplicates. *)
-let invalid_ids ~min ~max ~nmin ~nmax =
-  List.Cartesian_product.map2
-    (List.range ~start:`inclusive ~stop:`inclusive min max)
-    (List.range ~start:`inclusive ~stop:`inclusive nmin nmax)
-    ~f:(fun x n -> Option.some_if (is_invalid x ~n) x)
-  |> List.filter_map ~f:Fn.id
+let is_repeated_at_least_twice n =
+  (* This wonderful function comes from this StackOverflow question:
+     https://stackoverflow.com/questions/55823298/how-do-i-check-if-a-string-is-entirely-made-of-the-same-substring. *)
+  let s = Int.to_string n in
+  let ss = s ^ s in
+  String.is_substring (String.sub ss ~pos:1 ~len:(String.length ss - 2)) ~substring:s
 ;;
 
-let part1 input =
-  String.split input ~on:','
-  |> List.map ~f:parse_range
-  |> List.concat_map ~f:(fun (min, max) -> invalid_ids ~min ~max ~nmin:2 ~nmax:2)
-  |> List.sum (module Int) ~f:Fn.id
-;;
-
-let part2 input =
+let solve input part =
   String.split input ~on:','
   |> List.map ~f:parse_range
   |> List.concat_map ~f:(fun (min, max) ->
-    invalid_ids ~min ~max ~nmin:2 ~nmax:(count_digits max))
-  |> Hash_set.of_list (module Int)
-  |> Hash_set.sum (module Int) ~f:Fn.id
+    List.range ~start:`inclusive ~stop:`inclusive min max
+    |> List.filter
+         ~f:
+           (match part with
+            | `Part1 -> is_repeated_exactly_twice
+            | `Part2 -> is_repeated_at_least_twice))
+  |> List.sum (module Int) ~f:Fn.id
 ;;
 
-let%expect_test "n_digits" =
-  let examples = [ 1; 2; 9; 10; 111; 99999; 100000; 18925; 37848 ] in
-  Ascii_table.simple_list_table
-    [ "x"; "n_digits x" ]
-    (List.map examples ~f:(fun x -> [ x; count_digits x ] |> List.map ~f:Int.to_string));
-  [%expect
-    {|
-    ┌────────┬────────────┐
-    │      x │ n_digits x │
-    ├────────┼────────────┤
-    │      1 │          1 │
-    │      2 │          1 │
-    │      9 │          1 │
-    │     10 │          2 │
-    │    111 │          3 │
-    │  99999 │          5 │
-    │ 100000 │          6 │
-    │  18925 │          5 │
-    │  37848 │          5 │
-    └────────┴────────────┘
-    |}]
-;;
+let part1 input = solve input `Part1
+let part2 input = solve input `Part2
 
-let%expect_test "take_digits" =
-  let examples = [ 10, 1; 10, 2; 123, 1; 123, 2; 123, 3 ] in
-  Ascii_table.simple_list_table
-    [ "x"; "n"; "take_digits x ~n" ]
-    (List.map examples ~f:(fun (x, n) ->
-       [ x; n; take_digits x ~n ] |> List.map ~f:Int.to_string));
-  [%expect
-    {|
-    ┌─────┬───┬──────────────────┐
-    │   x │ n │ take_digits x ~n │
-    ├─────┼───┼──────────────────┤
-    │  10 │ 1 │                1 │
-    │  10 │ 2 │               10 │
-    │ 123 │ 1 │                1 │
-    │ 123 │ 2 │               12 │
-    │ 123 │ 3 │              123 │
-    └─────┴───┴──────────────────┘
-    |}]
-;;
-
-let%expect_test "repeat_digits" =
-  let examples = [ 10, 1; 10, 2; 123, 1; 123, 2; 123, 3 ] in
-  Ascii_table.simple_list_table
-    [ "x"; "n"; "repeat_digits x ~n" ]
-    (List.map examples ~f:(fun (x, n) ->
-       [ x; n; repeat_digits x ~n ] |> List.map ~f:Int.to_string));
-  [%expect
-    {|
-    ┌─────┬───┬────────────────────┐
-    │   x │ n │ repeat_digits x ~n │
-    ├─────┼───┼────────────────────┤
-    │  10 │ 1 │                 10 │
-    │  10 │ 2 │               1010 │
-    │ 123 │ 1 │                123 │
-    │ 123 │ 2 │             123123 │
-    │ 123 │ 3 │          123123123 │
-    └─────┴───┴────────────────────┘
-    |}]
-;;
-
-let%expect_test "is_invalid" =
-  let examples = [ 11, 2; 12, 2; 13, 2; 22, 2; 95, 2; 96, 2; 99, 2; 100, 2; 115, 2 ] in
-  Ascii_table.simple_list_table
-    [ "x"; "n"; "is_invalid x ~n" ]
-    (List.map examples ~f:(fun (x, n) ->
-       [ Int.to_string x; Int.to_string n; Bool.to_string (is_invalid x ~n) ]));
-  [%expect
-    {|
-    ┌─────┬───┬─────────────────┐
-    │   x │ n │ is_invalid x ~n │
-    ├─────┼───┼─────────────────┤
-    │  11 │ 2 │            true │
-    │  12 │ 2 │           false │
-    │  13 │ 2 │           false │
-    │  22 │ 2 │            true │
-    │  95 │ 2 │           false │
-    │  96 │ 2 │           false │
-    │  99 │ 2 │            true │
-    │ 100 │ 2 │           false │
-    │ 115 │ 2 │           false │
-    └─────┴───┴─────────────────┘
-    |}]
-;;
-
-let%expect_test "invalid_ids" =
-  let examples = [ 11, 22; 95, 115 ] in
-  Ascii_table.simple_list_table
-    [ "min"; "max"; "invalid_ids ~min ~max ~nmin:2 ~nmax:2" ]
-    (List.map examples ~f:(fun (min, max) ->
-       [ Int.to_string min
-       ; Int.to_string max
-       ; Sexp.to_string_hum
-           (List.sexp_of_t Int.sexp_of_t (invalid_ids ~min ~max ~nmin:2 ~nmax:2))
-       ]));
-  [%expect
-    {|
-    ┌─────┬─────┬───────────────────────────────────────┐
-    │ min │ max │ invalid_ids ~min ~max ~nmin:2 ~nmax:2 │
-    ├─────┼─────┼───────────────────────────────────────┤
-    │  11 │  22 │                               (11 22) │
-    │  95 │ 115 │                                  (99) │
-    └─────┴─────┴───────────────────────────────────────┘
-    |}];
-  (* A few samples from the example input for sanity checking. *)
+let%expect_test "is_repeated" =
   let examples =
-    [ 998, 1012, 2, 3; 2121212118, 2121212124, 2, 10; 824824821, 824824827, 2, 9 ]
+    [ 11
+    ; 111
+    ; 1112
+    ; 11121112
+    ; 121212
+    ; 12121212
+    ; 123123
+    ; 123123123
+    ; 123123123123
+    ; 127895
+    ; 123123124
+    ]
   in
   Ascii_table.simple_list_table
-    [ "min"; "max"; "nmin"; "nmax"; "invalid_ids ~min ~max ~nmin ~nmax" ]
-    (List.map examples ~f:(fun (min, max, nmin, nmax) ->
-       [ Int.to_string min
-       ; Int.to_string max
-       ; Int.to_string nmin
-       ; Int.to_string nmax
-       ; Sexp.to_string_hum
-           (List.sexp_of_t Int.sexp_of_t (invalid_ids ~min ~max ~nmin ~nmax))
+    [ "n"; "is_repeated_exactly_twice n"; "is_repeated_at_least_twice n" ]
+    (List.map examples ~f:(fun n ->
+       [ Int.to_string n
+       ; Bool.to_string (is_repeated_exactly_twice n)
+       ; Bool.to_string (is_repeated_at_least_twice n)
        ]));
   [%expect
     {|
-    ┌────────────┬────────────┬──────┬──────┬───────────────────────────────────┐
-    │        min │        max │ nmin │ nmax │ invalid_ids ~min ~max ~nmin ~nmax │
-    ├────────────┼────────────┼──────┼──────┼───────────────────────────────────┤
-    │        998 │       1012 │    2 │    3 │                        (999 1010) │
-    │ 2121212118 │ 2121212124 │    2 │   10 │                      (2121212121) │
-    │  824824821 │  824824827 │    2 │    9 │                       (824824824) │
-    └────────────┴────────────┴──────┴──────┴───────────────────────────────────┘
+    ┌──────────────┬─────────────────────────────┬──────────────────────────────┐
+    │            n │ is_repeated_exactly_twice n │ is_repeated_at_least_twice n │
+    ├──────────────┼─────────────────────────────┼──────────────────────────────┤
+    │           11 │                        true │                         true │
+    │          111 │                       false │                         true │
+    │         1112 │                       false │                        false │
+    │     11121112 │                        true │                         true │
+    │       121212 │                       false │                         true │
+    │     12121212 │                        true │                         true │
+    │       123123 │                        true │                         true │
+    │    123123123 │                       false │                         true │
+    │ 123123123123 │                        true │                         true │
+    │       127895 │                       false │                        false │
+    │    123123124 │                       false │                        false │
+    └──────────────┴─────────────────────────────┴──────────────────────────────┘
     |}]
 ;;
 
