726a9db444aec5fe0d47a23efebf0b9e3d9a7ee0
Christian Persson <582273+saser@users.noreply.github.com>
2025-12-09 09:33:50 +0100

    add solution for part 1

--- lib/year2025_day09.ml
+++ lib/year2025_day09.ml
@@ -15,7 +15,25 @@ let example_input =
 |}
 ;;
 
-let part1 _input = -1
+let part1 input =
+  let reds =
+    String.split_lines input
+    |> List.map ~f:(fun line ->
+      match String.split line ~on:',' with
+      | [ x; y ] -> Int.of_string x, Int.of_string y
+      | _ -> Printf.failwithf "invalid line: %S" line ())
+    |> List.to_array
+  in
+  let x =
+    Array.cartesian_product reds reds
+    |> Array.map ~f:(fun ((x1, y1), (x2, y2)) ->
+      (Int.abs (x1 - x2) + 1) * (Int.abs (y1 - y2) + 1))
+    |> Array.max_elt ~compare:Int.compare
+    |> Option.value_exn
+  in
+  x
+;;
+
 let part2 _input = -1
 
 let%expect_test "solution" =
@@ -24,13 +42,15 @@ let%expect_test "solution" =
     printf "part2: %d\n" (part2 input)
   in
   test example_input;
-  [%expect {|
-    part1: -1
+  [%expect
+    {|
+    part1: 50
     part2: -1
     |}];
   test Inputs.year2025_day09;
-  [%expect {|
-    part1: -1
+  [%expect
+    {|
+    part1: 4763932976
     part2: -1
     |}]
 ;;
