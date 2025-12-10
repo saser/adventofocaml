056e55496653b9ad86989183f5ec5b1c07d9b6c5
Christian Persson <582273+saser@users.noreply.github.com>
2025-12-10 09:57:54 +0100

    scaffolding

---
+++ lib/year2025_day10.ml
@@ -0,0 +1,26 @@
+open Base
+open Stdio
+
+let part1 _input = -1
+let part2 _input = -2
+
+let example_input =
+  String.strip
+    {|
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+|}
+;;
+
+let%expect_test "solution" =
+  let test input =
+    printf "part1: %d\n" (part1 input);
+    printf "part2: %d\n" (part2 input)
+  in
+  test example_input;
+  [%expect {|
+    part1: -1
+    part2: -2
+    |}]
+;;
